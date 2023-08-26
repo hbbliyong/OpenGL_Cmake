cc5d91bdb7883fc1ed78404b143f41fc2fc8a7bb
李勇(10067418) <liy-dn@glodon.com>
2023-08-26 15:54:58 +0800

    init commit

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/src/glad.c.obj"
+  "CMakeFiles/hello.dir/src/glad.c.obj.d"
+  "CMakeFiles/hello.dir/src/main.cpp.obj"
+  "CMakeFiles/hello.dir/src/main.cpp.obj.d"
+  "hello.exe"
+  "hello.exe.manifest"
+  "hello.pdb"
+  "libhello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
